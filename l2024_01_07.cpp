7c01a919e17c0e843c72337194c66ef77f6141bc
“van18615302908” <18615302908@163.com>
2024-01-10 20:36:43 +0800

    1

---
+++ l2024_01_07.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string>
+
+int main(){
+    // char ss[10]="12345",
+    // gets(ss);strcat(ss,"6789"); printf("%s",ss);
+    // return 0;
+}
+
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-//111111111111111
-using namespace std;
-
-int main() {
-    cout << "c++主文件运行成功\n" << endl;
-    return 0;
-}
