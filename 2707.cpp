ae5f76db909956522ca0d8b4da886252b02365de
van18615302908 <18615302908@163.com>
2024-01-11 20:42:27 +0800

    2707

---
+++ 2707.cpp
@@ -0,0 +1,34 @@
+//给你一个下标从 0 开始的字符串 s 和一个单词字典 dictionary 。你需要将 s 分割成若干个 互不重叠 的子字符串，每个子字符串都在 dictionary 中出现过。s 中可能会有一些 额外的字符 不在任何子字符串中。
+//s = "leetscode", dictionary = ["leet","code","leetcode"]\
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        vector<int> d(n + 1, INT_MAX);
+        unordered_map<string, int> calculated;
+        for (auto a : dictionary) {
+            calculated[a]++;
+        }
+        d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (calculated.count(s.substr(j, i - j))) {
+                    d[i] = min(d[i], d[j]);
+                }
+            }
+        }
+        return d[n];
+    }
+};
+
+int main(){
+    return 0;
+}
